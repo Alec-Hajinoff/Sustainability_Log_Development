ab6bbd658075d10f56f2585cefdc6dd04a0d67f3
Alec Hajinoff <hajinoff@gmail.com>
2025-10-12 11:06:18 +0100

    12.10.25, 11.06

--- React/src/App.js
+++ React/src/App.js
@@ -6,7 +6,6 @@ import Header from "./Header";
 import MainRegLog from "./MainRegLog";
 import Footer from "./Footer";
 import RegisteredPage from "./RegisteredPage";
-import AccountPage from "./AccountPage";
 import CreateAction from "./CreateAction";
 import LogoutComponent from "./LogoutComponent";
 import CompanySearch from "./CompanySearch"; // This is the page that does not require sign in and is used by the counter signer
@@ -19,7 +18,6 @@ function App() {
         <Routes>
           <Route path="/" element={<MainRegLog />} />
           <Route path="RegisteredPage" element={<RegisteredPage />} />
-          <Route path="AccountPage" element={<AccountPage />} /> 
           <Route path="CreateAction" element={<CreateAction />} />
           <Route path="LogoutComponent" component={LogoutComponent} />
           <Route path="CompanySearch" element={<CompanySearch />} /> {/*This is the page that does not require sign in and is used by the counter signer*/}
