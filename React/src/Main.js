2254d8a2fd48115b6fb08c49d7499ba9c5f29cb8
Alec Hajinoff <hajinoff@gmail.com>
2025-11-19 16:53:31 +0000

    19.11.25, 16.53

--- React/src/Main.js
+++ React/src/Main.js
@@ -4,7 +4,7 @@ import "./Main.css";
 function Main() {
   return (
     <div className="container">
-      <div className="row">
+      <div className="row mb-4">
         <div className="col-12">
           <p>
             Sustainability Log is a web application that helps ethical
@@ -14,32 +14,34 @@ function Main() {
         </div>
       </div>
 
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <div className="text-start">
-            <ul className="list-unstyled ps-0 mb-0">
-              <li>Step 1: Log your sustainability actions</li>
-              <li>
-                Step 2: Add Digital Product Passport information where relevant
-              </li>
-              <li>Step 3: Link suppliers as required</li>
-              <li>Step 4: Automatically anchored on the blockchain</li>
-              <li>
-                Step 5: Share your sustainability story with stakeholders via
-                URL or QR code
-              </li>
-            </ul>
-          </div>
+      <div className="row mb-4">
+        <div className="col-12 text-start">
+          <ul className="list-unstyled ps-0 mb-0">
+            <li className="step step-1">
+              Step 1: Log your sustainability actions
+            </li>
+            <li className="step step-2">
+              Step 2: Add Digital Product Passport information where relevant
+            </li>
+            <li className="step step-3">Step 3: Link suppliers as required</li>
+            <li className="step step-4">
+              Step 4: Automatically anchored on the blockchain
+            </li>
+            <li className="step step-5">
+              Step 5: Share your sustainability story with stakeholders via URL
+              or QR code
+            </li>
+          </ul>
         </div>
+      </div>
 
-        <div className="col-12 col-md-6">
-          <div className="card">
-            <img
-              src="/TimelineSample.png"
-              className="card-img-top img-fluid"
-              alt="Sustainability timeline"
-            />
-          </div>
+      <div className="row mb-4">
+        <div className="col-12">
+          <img
+            src="/TimelineSample.png"
+            className="img-fluid w-100"
+            alt="Sustainability timeline"
+          />
         </div>
       </div>
     </div>
